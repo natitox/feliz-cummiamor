de2febea55865cb51b8b0d5b4e97dc25fe55280b
natitox <renatorebolledo1@gmail.com>
2026-03-24 21:02:02 -0300

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -11,14 +11,7 @@
    7. Copia los valores y pégalos abajo
 ═══════════════════════════════════════════════════════ */
 
-const FIREBASE_CONFIG = {
-  apiKey:            "AIzaSyBspoBUsOIr_BoDyW3zWntRenu2aqXJaZ0",
-  authDomain:        "cartasnupi.firebaseapp.com",
-  projectId:         "cartasnupi",
-  storageBucket:     "cartasnupi.firebasestorage.app",
-  messagingSenderId: "780189476433",
-  appId:             "1:780189476433:web:1126b03a855a4e8b7dba70"
-};
+
 
 /* ═══════════════════════════════════════════════════════
    USUARIOS PERMITIDOS
