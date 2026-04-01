075c1cec56056d273190e245f948240a3ab06f05
natitox <renatorebolledo1@gmail.com>
2026-04-01 17:47:58 -0300

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -1,3 +1,4 @@
+
 const FIREBASE_CONFIG = {
   apiKey:            "AIzaSyABfB3ZeItipmcMGzmSkRqMT-B6Vz0Hn8I",
   authDomain:        "cartasnupi.firebaseapp.com",
@@ -6,3 +7,14 @@ const FIREBASE_CONFIG = {
   messagingSenderId: "780189476433",
   appId:             "1:780189476433:web:1126b03a855a4e8b7dba70"
 };
+
+/* Alias públicos solo para mantener el login bonito */
+const USERNAME_TO_EMAIL = Object.freeze({
+  snupi:  "snupi@amor.app",
+  natito: "natito@amor.app"
+});
+
+const ALLOWED_EMAILS = Object.freeze(Object.values(USERNAME_TO_EMAIL));
+
+/* Esto no protege datos reales; solo desbloquea una sección visual */
+const SECRET_PIN = "20061709";
