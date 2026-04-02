cb23e101ae7e1ac1def490a9a2d4d8d2fa42980e
natitox <renatorebolledo1@gmail.com>
2026-04-02 03:10:35 -0300

    Update script.js

--- script.js
+++ script.js
@@ -469,7 +469,7 @@ function renderPuzzle(size) {
 
     cell.style.width               = cellPx + 'px';
    cell.style.height              = cellPx + 'px';
-   cell.style.backgroundImage = 'url("img/enamoradito.jpeg")';
+   cell.style.backgroundImage = 'url("img/enamoradito.jpg")';
    cell.style.backgroundSize      = `${size * 100}% ${size * 100}%`;
    cell.style.backgroundRepeat    = 'no-repeat';
    cell.style.backgroundPositionX = `${bgPosX}%`;
