651e85694aca0fdeb64c6267c631725ba8b0b405
natitox <renatorebolledo1@gmail.com>
2026-04-06 02:17:17 -0400

    Update script.js

--- script.js
+++ script.js
@@ -10,7 +10,7 @@
    CONFIGURACIÓN — EDITA AQUÍ
 ═══════════════════════════════════════ */
 const CONFIG = {
-  PIN: '7195',
+  PIN: '1805',
 
   EMAILJS_SERVICE_ID:  'service_60wzk5j',   // ← tu service ID
   EMAILJS_TEMPLATE_ID: 'template_50p5kvb',  // ← tu template ID
